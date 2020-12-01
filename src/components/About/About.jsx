1ad34768971d5dfeb1e81caf789f58f278370920
Zemen Teshome <zementeshome@gmail.com>
2020-12-01 18:25:30 -0500

    playing with scrollintoview in aboutcomponenet

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,15 +2,36 @@ import React from 'react'
 import Header from './../Header/Header';
 import './About.scss';
 
-function About() {
+class About extends React.Component {
+
+myDivToFocus = React.createRef()
+
+handleOnClick = (e) => {
+    let div = document.getElementsByClassName("about__text")
+    div = {display: 'block'};
+    if(this.myDivToFocus.current) {
+        this.myDivToFocus.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest'
+        })
+    }
+}
+render() {
     return (
         <section className="about">
             <Header />
         <div className="about__image-container">
         <img className="about__image" src={process.env.PUBLIC_URL + 'assets/images/memoji-laptop.png'} alt="zemen memoji"/>
         </div>
+        <button className="about__button" onClick={this.handleOnClick}>Click me</button>
+            <div className="about__text" ref={this.myDivToFocus} style={{display: 'none'}}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                {/* <p className="about__text">Hello</p> */}
+            </div>
         </section>
     )
+}
+
 }
 
 export default About;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './../Header/Header';
+import './Contact.scss';
 
 function Contact() {
-    return (
-        <section className="contact">
-            <Header />
-        <h1>Contact</h1>
-        </section>
-    )
-}
+        return (
+            <section className="contact">
+                <Header />
+            <img className="contact__image" src={process.env.PUBLIC_URL + '/assets/images/v1.svg'} alt="background"/>
+            </section>
+        )
+    }
 
 export default Contact;
