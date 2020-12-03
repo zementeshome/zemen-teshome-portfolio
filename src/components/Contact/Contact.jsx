0f27410cf3a335a44cd6606b2dc58c0eb58bd5cb
Zemen Teshome <zementeshome@gmail.com>
2020-12-02 21:56:10 -0500

    information for contact page added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,13 @@ function Contact() {
         return (
             <section className="contact">
                 <Header />
+                <h1 className="contact__header">CONTACT</h1>
+                <h2 className="contact__sub-heading">EMAIL</h2>
+                <p className="contact__info">zementeshome@gmail.com</p>
+                <h2 className="contact__sub-heading">LINKEDIN</h2>
+                <p className="contact__info">linkedin.com/zementeshome</p>
+                <h2 className="contact__sub-heading">GITHUB</h2>
+                <p className="contact__info">github.com/zementeshome</p>
             <img className="contact__image" src={process.env.PUBLIC_URL + '/assets/images/v1.svg'} alt="background"/>
             </section>
         )
