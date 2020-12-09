f5b0a7211e320129d7b7530b510fbfd1146bf615
Zemen Teshome <zementeshome@gmail.com>
2020-12-09 10:30:22 -0500

    beginning of styling for contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,13 +6,16 @@ function Contact() {
         return (
             <section className="contact">
                 <Header />
+                <div className="contact__container">
                 <h1 className="contact__header">CONTACT</h1>
-                <h2 className="contact__sub-heading">EMAIL</h2>
+                <p className="contact__message">drop me a line</p>
+                {/* <h2 className="contact__sub-heading">EMAIL</h2>
                 <p className="contact__info">zementeshome@gmail.com</p>
                 <h2 className="contact__sub-heading">LINKEDIN</h2>
                 <p className="contact__info">linkedin.com/zementeshome</p>
                 <h2 className="contact__sub-heading">GITHUB</h2>
-                <p className="contact__info">github.com/zementeshome</p>
+                <p className="contact__info">github.com/zementeshome</p> */}
+                </div>
             {/* <img className="contact__image" src={process.env.PUBLIC_URL + '/assets/images/v1.svg'} alt="background"/> */}
             </section>
         )
