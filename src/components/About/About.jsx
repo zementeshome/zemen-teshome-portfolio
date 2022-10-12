24340d85e71a9d1ae8778270b32b8a903697afd5
Zemen Teshome <zementeshome@gmail.com>
2022-10-12 12:34:33 -0400

    updated portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,123 +1,179 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Header from './../Header/Header';
-import './About.scss';
-import gsap from 'gsap';
-import  { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { Link as ScrollLink } from 'react-scroll';
-import { animateScroll as scroll} from 'react-scroll'
-import AboutImage from '../AboutImage/AboutImage';
-import { FaHtml5 } from 'react-icons/fa';
-import { FaCss3Alt } from 'react-icons/fa';
-import { FaSass } from 'react-icons/fa';
-import { FaJs } from 'react-icons/fa';
-import { FaReact } from 'react-icons/fa';
-import { FaAngular } from 'react-icons/fa';
-import { FaNodeJs } from 'react-icons/fa';
-import { SiFirebase } from 'react-icons/si';
-import { DiMongodb } from 'react-icons/di';
-import { SiMysql } from 'react-icons/si';
-import { FaGithub } from 'react-icons/fa';
-import { FaTerminal } from 'react-icons/fa';
-import Arrow from '../../BackgroundImage/iconmonstr-arrow-66.svg';
+import React, { useRef, useEffect, useState } from "react";
+import Header from "./../Header/Header";
+import "./About.scss";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Link as ScrollLink } from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
+import AboutImage from "../AboutImage/AboutImage";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiFirebase } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
+import { SiJest } from "react-icons/si";
+import { SiStorybook } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaTerminal } from "react-icons/fa";
+import Arrow from "../../BackgroundImage/iconmonstr-arrow-66.svg";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const sections = [
-    {
-        title: 'ABOUT ME',
-        subtitle: "hi, i'm zemen and i build things. i'm a full stack developer with a professional background in public relations and communications. i recently made the transition into tech and i've been loving every second of it. i'm passionate about building user-friendly, accessible, nice-to-look at websites. when i'm not coding, i'm usually digging for music."
-    },
-    {
-        title: 'SKILLS',
-        subtitle: ''    
-    }
-]
+  {
+    title: "ABOUT ME",
+    subtitle:
+      "hi, i'm zemen and i build things. i'm a software developer with a professional background in public relations and communications. i'm passionate about building user-friendly, accessible at components. when i'm not coding, i'm usually digging for music.",
+  },
+  {
+    title: "SKILLS",
+    subtitle: "",
+  },
+];
 
 function About() {
-    const [background, ] = useState('#000000');
+  const [background] = useState("#000000");
 
-    const aboutRef = useRef(null)
-    const revealRefs = useRef([]);
-    revealRefs.current = [];
+  const aboutRef = useRef(null);
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
 
-    useEffect(() => {
-        gsap.to(aboutRef.current, {
-            duration: 1,
-            backgroundColor: background,
-            ease: 'ease'
-        });
-    }, [background])
+  useEffect(() => {
+    gsap.to(aboutRef.current, {
+      duration: 1,
+      backgroundColor: background,
+      ease: "ease",
+    });
+  }, [background]);
 
-    useEffect(() => {
-        gsap.from(aboutRef.current, {
-            duration: 1,
-            autoAlpha: 0,
-            ease: 'none',
-            delay: 1
-        });
+  useEffect(() => {
+    gsap.from(aboutRef.current, {
+      duration: 1,
+      autoAlpha: 0,
+      ease: "none",
+      delay: 1,
+    });
 
     revealRefs.current.forEach((el, index) => {
-        gsap.fromTo(el, {
-            autoAlpha: 0
-        }, {
-            duration: 1,
-            autoAlpha: 1,
-            ease: 'none',
-            scrollTrigger: {
-                id: `section-${index+1}`,
-                trigger: el,
-                start: 'top center+=100',
-                toggleActions: 'play none none reverse'
-            }
-        });
-    })
-    }, [])
+      gsap.fromTo(
+        el,
+        {
+          autoAlpha: 0,
+        },
+        {
+          duration: 1,
+          autoAlpha: 1,
+          ease: "none",
+          scrollTrigger: {
+            id: `section-${index + 1}`,
+            trigger: el,
+            start: "top center+=100",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+    });
+  }, []);
 
-    const addToRefs = (el) => {
-        if (el && !revealRefs.current.includes(el)) {
-            revealRefs.current.push(el)
-        }  
-    };
-    
-    return(
-        <section className="about">
-            <Header/>
-        <div className="about__image-container" ref={aboutRef}>
-            <AboutImage />
-            <ScrollLink className="about__scroll" to="me" smooth={true} duration={1000}><img className="about__arrow" src={process.env.PUBLIC_URL + '/assets/images/iconmonstr-arrow-65.svg'} alt=""/></ScrollLink>
-        </div>
-        <section className="about__section-2" id="me">
-            {
-                sections.map(({title, subtitle}) => {
-                    return (
-                        <div key={title} className="about__text-container" ref={addToRefs}>
-                            <h2 className="about__header">{title}</h2>
-                            <div className="about__text-box">
-                            <p className="about__text">{subtitle}</p>
-                            </div>
-                        </div>
-                    )
-                })
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  };
+
+  return (
+    <section className="about">
+      <Header />
+      <div className="about__image-container" ref={aboutRef}>
+        <AboutImage />
+        <ScrollLink
+          className="about__scroll"
+          to="me"
+          smooth={true}
+          duration={1000}
+        >
+          <img
+            className="about__arrow"
+            src={
+              process.env.PUBLIC_URL + "/assets/images/iconmonstr-arrow-65.svg"
             }
-            <div className="about__skills" ref={addToRefs}>
-                <div className="about__flex">
-                <p className="about__skills-icon"><FaHtml5 /></p>
-                <p className="about__skills-icon"><FaCss3Alt /> </p>
-                <p className="about__skills-icon"><FaSass /> </p>
-                <p className="about__skills-icon"> <FaJs /> </p>
-                <p className="about__skills-icon"> <FaReact /> </p>
-                <p className="about__skills-icon"> <FaAngular /> </p>
-                <p className="about__skills-icon"><FaNodeJs /> </p>
-                <p className="about__skills-icon"><SiFirebase /></p>
-                <p className="about__skills-icon"><DiMongodb /></p>
-                <p className="about__skills-icon"><SiMysql /></p>
-                <p className="about__skills-icon"> <FaGithub /></p>
-                <p className="about__skills-icon"><FaTerminal /></p>
-                </div>
+            alt=""
+          />
+        </ScrollLink>
+      </div>
+      <section className="about__section-2" id="me">
+        {sections.map(({ title, subtitle }) => {
+          return (
+            <div key={title} className="about__text-container" ref={addToRefs}>
+              <h2 className="about__header">{title}</h2>
+              <div className="about__text-box">
+                <p className="about__text">{subtitle}</p>
+              </div>
             </div>
-                <img className="about__scroll-top" ref={aboutRef} src={Arrow} alt="arrow" onClick={() => {scroll.scrollToTop()}}/>
-          </section>
-     </section>
-    )
+          );
+        })}
+        <div className="about__skills" ref={addToRefs}>
+          <div className="about__flex">
+            <p className="about__skills-icon">
+              <FaHtml5 />
+            </p>
+            <p className="about__skills-icon">
+              <FaCss3Alt />{" "}
+            </p>
+            <p className="about__skills-icon">
+              <FaSass />{" "}
+            </p>
+            <p className="about__skills-icon">
+              {" "}
+              <FaJs />{" "}
+            </p>
+            <p className="about__skills-icon">
+              {" "}
+              <FaReact />{" "}
+            </p>
+            <p className="about__skills-icon">
+              {" "}
+              <SiTypescript />{" "}
+            </p>
+            <p className="about__skills-icon">
+              <FaNodeJs />{" "}
+            </p>
+            <p className="about__skills-icon">
+              <SiFirebase />
+            </p>
+            <p className="about__skills-icon">
+              <SiRedux />
+            </p>
+            <p className="about__skills-icon">
+              <SiJest />
+            </p>
+            <p className="about__skills-icon">
+              <SiStorybook />
+            </p>
+            <p className="about__skills-icon">
+              {" "}
+              <FaGithub />
+            </p>
+            <p className="about__skills-icon">
+              <FaTerminal />
+            </p>
+          </div>
+        </div>
+        <img
+          className="about__scroll-top"
+          ref={aboutRef}
+          src={Arrow}
+          alt="arrow"
+          onClick={() => {
+            scroll.scrollToTop();
+          }}
+        />
+      </section>
+    </section>
+  );
 }
 export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,58 +1,134 @@
-import React from 'react';
-import Header from '../Header/Header';
-import './Home.scss';
-import $ from 'jquery';
-import Footer from '../Footer/Footer';
-import { Link as ScrollLink } from 'react-scroll';
-import { animateScroll as scroll} from 'react-scroll';
-import Arrow from '../../BackgroundImage/iconmonstr-arrow-66.svg';
+import React from "react";
+import Header from "../Header/Header";
+import "./Home.scss";
+import $ from "jquery";
+import Footer from "../Footer/Footer";
+import { Link as ScrollLink } from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
+import Arrow from "../../BackgroundImage/iconmonstr-arrow-66.svg";
 
 function Home() {
+  $(function () {
+    $(".intro").addClass("go");
 
-    $(function() {
-        $('.intro').addClass('go');
-      
-        $('.reload').click(function() {
-          $('.intro').removeClass('go').delay(200).queue(function(next) {
-            $('.intro').addClass('go');
-            next();
-          });
-      
+    $(".reload").click(function () {
+      $(".intro")
+        .removeClass("go")
+        .delay(200)
+        .queue(function (next) {
+          $(".intro").addClass("go");
+          next();
         });
-      })
+    });
+  });
 
-    return (
-      <section className="home">
-            <Header />
-        <div className="container">
-           <svg className="intro">
-              <text text-anchor="start" x="10" y="200" className="text text-stroke" clip-path="url(#text1)" style={{fontFamily: 'Aileron Thin'}}>ZEMEN TESHOME</text>
-              <text text-anchor="start" x="40" y="250" className="text text-stroke-2" clip-path="url(#text2)">FULL STACK DEVELOPER</text>
-              <text text-anchor="start" x="10" y="300" className="text text-stroke-2" clip-path="url(#text3)">BASED IN TORONTO</text>
-              <text text-anchor="start" x="10" y="200" className="text text-stroke text-stroke-2" clip-path="url(#text1)">ZEMEN TESHOME</text>
-              <text text-anchor="start" x="40" y="250" className="text text-stroke text-stroke-2" clip-path="url(#text2)">FULL STACK DEVELOPER</text>
-              <text text-anchor="start" x="10" y="300" className="text text-stroke text-stroke-2" clip-path="url(#text3)">BASED IN TORONTO</text> 
+  return (
+    <section className="home">
+      <Header />
+      <div className="container">
+        <svg className="intro">
+          <text
+            text-anchor="start"
+            x="10"
+            y="200"
+            className="text text-stroke"
+            clip-path="url(#text1)"
+            style={{ fontFamily: "Aileron Thin" }}
+          >
+            ZEMEN TESHOME
+          </text>
+          <text
+            text-anchor="start"
+            x="40"
+            y="250"
+            className="text text-stroke-2"
+            clip-path="url(#text2)"
+          >
+            SOFTWARE DEVELOPER
+          </text>
+          <text
+            text-anchor="start"
+            x="10"
+            y="300"
+            className="text text-stroke-2"
+            clip-path="url(#text3)"
+          >
+            BASED IN TORONTO
+          </text>
+          <text
+            text-anchor="start"
+            x="10"
+            y="200"
+            className="text text-stroke text-stroke-2"
+            clip-path="url(#text1)"
+          >
+            ZEMEN TESHOME
+          </text>
+          <text
+            text-anchor="start"
+            x="40"
+            y="250"
+            className="text text-stroke text-stroke-2"
+            clip-path="url(#text2)"
+          >
+            FULL STACK DEVELOPER
+          </text>
+          <text
+            text-anchor="start"
+            x="10"
+            y="300"
+            className="text text-stroke text-stroke-2"
+            clip-path="url(#text3)"
+          >
+            BASED IN TORONTO
+          </text>
           <defs>
-          <clipPath id="text1">
-              <text text-anchor="start" x="10" y="200" className="text">ZEMEN TESHOME</text>
-          </clipPath>
-          <clipPath id="text2">
-              <text text-anchor="start" x="40" y="250" className="text">FULL STACK DEVELOPER</text>
-          </clipPath> 
-          <clipPath id="text3">
-              <text text-anchor="start" x="10" y="300" className="text">BASED IN TORONTO</text>
-          </clipPath> 
-        </defs>
-      </svg>
-</div>
-      <ScrollLink className="home__scroll" to="footer" smooth={true} duration={1000}><img className="home__arrow" src={process.env.PUBLIC_URL + '/assets/images/iconmonstr-arrow-65.svg'} alt="arrow"/></ScrollLink>
-  <div classname="home__background-container">
-   <div className="home__background-image"></div>
-  </div>
-      <img className="home__arrow-top" src={Arrow} alt="arrow" onClick={() => {scroll.scrollToTop()}}/>
+            <clipPath id="text1">
+              <text text-anchor="start" x="10" y="200" className="text">
+                ZEMEN TESHOME
+              </text>
+            </clipPath>
+            <clipPath id="text2">
+              <text text-anchor="start" x="40" y="250" className="text">
+                FULL STACK DEVELOPER
+              </text>
+            </clipPath>
+            <clipPath id="text3">
+              <text text-anchor="start" x="10" y="300" className="text">
+                BASED IN TORONTO
+              </text>
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      <ScrollLink
+        className="home__scroll"
+        to="footer"
+        smooth={true}
+        duration={1000}
+      >
+        <img
+          className="home__arrow"
+          src={
+            process.env.PUBLIC_URL + "/assets/images/iconmonstr-arrow-65.svg"
+          }
+          alt="arrow"
+        />
+      </ScrollLink>
+      <div classname="home__background-container">
+        <div className="home__background-image"></div>
+      </div>
+      <img
+        className="home__arrow-top"
+        src={Arrow}
+        alt="arrow"
+        onClick={() => {
+          scroll.scrollToTop();
+        }}
+      />
       <Footer />
-  </section>
-  )
+    </section>
+  );
 }
 
 export default Home;
